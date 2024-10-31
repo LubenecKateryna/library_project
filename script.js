2fdf905536624e7f51118629cf5ea886d46c0f42
Kate1232113 <lubeneckatia@gmail.com>
2024-10-31 22:16:26 +0200

    add style_for_projects scroll_to_top cards_withThree_btn

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
+/*HIDDEN MENU */
 const hiddenMenu = document.getElementById("lefthiddemMenuBox");
 const mainMenuBtn = document.getElementById("header-menu-btn");
 
 mainMenuBtn.addEventListener("click", function toggleMenu() {
-    hiddenMenu.style.width = hiddenMenu.style.width === "250px" ? "0" : "250px";
+    if (hiddenMenu.style.width === "15%") {
+        hiddenMenu.style.width = "0";
+    } else {
+        hiddenMenu.style.width = "15%";
+    }
 });
 
 document.addEventListener("click", (event) => {
@@ -12,7 +17,7 @@ document.addEventListener("click", (event) => {
     }
 });
 
-// Скрытие и показ аккаунта
+/*HIDDEN ACCOUNT */
 const accountIcons = document.querySelectorAll(".class_account");
 const hiddenAccount = document.getElementById("hidden-account");
 const saveBtn = document.getElementById("hidden-input-btn");
@@ -30,8 +35,11 @@ saveBtn.addEventListener("click", () => {
     hiddenAccount.style.display = "none";
 });
 
-// Функция создания карточки товара
+/*PRODUCTS */
 function getProductHtml(product) {
+    const isAvailable = product.Vnayawnist && !product.NeVnayawnist;
+    const isUnavailable = !product.Vnayawnist && product.NeVnayawnist;
+
     return `
         <div class="product-card">
             <div class="catalog-item-top">
@@ -52,8 +60,11 @@ function getProductHtml(product) {
                     <span class="NeVnayavnist-text">${product.NeVnayawnist}</span>
                 </div>
                 <div class="item-bottom-btns">
-                    <button class="btn-to-basket">В корзину</button>
-                    <button class="btn-to-buy">Купить</button>
+                    <div class="botton-btn-VNAyavnosyi" ${isUnavailable ? 'style="display: none;"' : ''}>
+                        <span class="btn-to-basket">${product.btnToBasket}</span>
+                        <span class="btn-to-buy">${product.btnBuy}</span>
+                    </div>
+                    <span class="btn-wait" ${isAvailable ? 'style="display: none;"' : ''}>${product.btnWait}</span>
                 </div>
             </div>
         </div>
@@ -89,7 +100,32 @@ getProducts().then(products => {
     });
 });
 
-/*footer-conect-icon */
+/*HIDDEN BASCKET */
+
+/*GREETING_SCROLL*/
+const hiddenElement = document.getElementById("first-junr-books");
+const btn = document.querySelector(".greeting-content");
+
+function handleButtonClick() {
+  hiddenElement.scrollIntoView({ block: "center"});
+}
+
+btn.addEventListener("click", handleButtonClick);
+
+/*SCROLL_TO_TOP */
+const main_scroll_to_top = document.querySelector('.scroll_to_top_button')
+window.addEventListener('scroll', function(){
+    if(window.scrollY < 500){
+        document.querySelector(".scroll_to_top_button").classList.remove("scroll_btn_visibility")
+    }else{
+        main_scroll_to_top.classList.add("scroll_btn_visibility")
+    }
+})
+document.querySelector(".scroll_to_top_button").onclick = function() {
+    window.scrollTo( 0, 0)
+}
+
+/*FOOTER ICONS */
 function toggleDropdown() {
     const dropdown = document.querySelector('.footer-juners-a');
     dropdown.style.display = dropdown.style.display === 'block' ? 'none' : 'block';
